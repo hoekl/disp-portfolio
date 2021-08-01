2b61f05af156573230013eb0a42a213979770f82
l2-hoek <laurenz2.hoek@live.uwe.ac.uk>
2021-08-01 18:25:07 +0100

    Implement simplified deployed bpm process with service task executed in java

--- java/src/main/java/com/camunda/myBPM/org/camunda/bpm/engine/delegate/CheckCreditDelegate.java
+++ java/src/main/java/com/camunda/myBPM/org/camunda/bpm/engine/delegate/CheckCreditDelegate.java
@@ -1,5 +1,15 @@
 package com.camunda.myBPM.org.camunda.bpm.engine.delegate;
+import java.util.Random;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+public class CheckCreditDelegate implements JavaDelegate {
+    @Override
+    public void execute(DelegateExecution execution) throws Exception {
+        int min = 200;
+        int max = 1000;
+        Random rand = new Random();
+        execution.setVariable("creditScore", rand.nextInt(max - min) + min);
+        execution.setVariable("creditRefer", rand.nextBoolean());
 
-public class CheckCreditDelegate {
-
+    }
 }
