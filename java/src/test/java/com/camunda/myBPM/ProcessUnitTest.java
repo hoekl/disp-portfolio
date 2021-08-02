aa6f5d32a8d3d01ee5d598eef7afab536f828fde
l2-hoek <laurenz2.hoek@live.uwe.ac.uk>
2021-08-02 11:37:35 +0100

    Add unit test cases to check process is flowing correctly. Also add test to check tasks are resolved correctly, leading the process to finish

--- java/src/test/java/com/camunda/myBPM/ProcessUnitTest.java
+++ java/src/test/java/com/camunda/myBPM/ProcessUnitTest.java
@@ -1,15 +1,19 @@
 package com.camunda.myBPM;
 
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.init;
-
 import org.apache.ibatis.logging.LogFactory;
+import org.camunda.bpm.engine.impl.persistence.entity.TaskEntity;
+import org.camunda.bpm.engine.runtime.ProcessInstanceWithVariables;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.assertions.TaskAssert;
 import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+
+
 
 public class ProcessUnitTest {
     @ClassRule
@@ -36,6 +40,75 @@ public class ProcessUnitTest {
     public void testParsingAndDeployment() {
         // nothing is done here, as we just want to check for exceptions during
         // deployment
+    }
+    @Test
+    @Deployment(resources = "process.bpmn")
+    public void testCurrentStatus() {
+
+        // Obtain test run of BPMN
+        ProcessInstanceWithVariables processInstance =
+                (ProcessInstanceWithVariables) processEngine()
+                        .getRuntimeService()
+                        .startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+
+        // Obtain the value of the weatherOK variable
+        int creditScore = (int) processInstance.getVariables()
+                .get("creditScore");
+        System.out.println("creditScore: " + creditScore);
+
+        // Obtain a reference to the current task
+        TaskAssert task = assertThat(processInstance).task();
+
+        if (creditScore < 400) {
+            assertThat(processInstance)
+                    .isWaitingAt("Activity_1y9vtcf");
+            task.hasName("Inform customer");
+        } else if (creditScore >= 400 && creditScore <600){
+            assertThat(processInstance)
+                    .isWaitingAt("Activity_0k6byvc");
+            task.hasName("Call credit assessment");
+
+        } else if (creditScore >= 600) {
+            assertThat(processInstance).isWaitingAt("Activity_10a8ean");
+            task.hasName("Sell phone");
+        }
+        task.isNotAssigned();
+
+    }
+    @Test
+    @Deployment(resources = "process.bpmn")
+    public void testCompletionTask() {
+
+        // Obtain test run of BPMN
+        ProcessInstanceWithVariables processInstance =
+                (ProcessInstanceWithVariables) processEngine()
+                        .getRuntimeService()
+                        .startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+
+        // Obtain a reference to the current task
+        TaskAssert taskAssert =
+                assertThat(processInstance).task();
+        int creditScore = (int) processInstance.getVariables()
+                .get("creditScore");
+        System.out.println("creditScore: " + creditScore);
+
+        boolean creditRefer = (boolean) processInstance.getVariables().get("creditRefer");
+        System.out.println("creditRefer: " + creditRefer);
+
+        TaskEntity task = (TaskEntity)taskAssert.getActual();
+        task.delegate("user");
+
+        task.resolve();
+
+        if (creditScore >= 400 && creditScore < 600) {
+            TaskEntity secondTask = (TaskEntity)taskAssert.getActual();
+            secondTask.delegate("user");
+
+            secondTask.resolve();
+        }
+
+        assertThat(processInstance.isEnded());
+
     }
 
 
